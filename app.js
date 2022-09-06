507283f9aba00b3fac996f56a042f65d885ec34c
tonyxuan2021 <tonyyexuan@gmail.com>
2022-09-05 19:19:46 -0700

    create file

---
+++ app.js
@@ -0,0 +1,9 @@
+let person = new Object();
+
+person["firstname"] = "Tony";
+person["lastname"] = "Alicea";
+
+let firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person[firstNameProperty]);
